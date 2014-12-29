d7057ed675bd8c7a1ae2b2c41313b3f55cc493ba
Fabian Stevens Varon <fabianchoxd@gmail.com>
2014-12-29 17:56:11 -0500

    Commit-2 -- Final Document
    works fine usign XAMMP
    
    Search at RealTime  Using Ajax + php.

--- servidor.php
+++ servidor.php
@@ -28,6 +28,7 @@
 		'Fontana',
 		'Gerardo',
 		'German',
+		'hernando',
 		'Ivan',
 		'Irvin',
 		'Ivanna',
@@ -48,7 +49,89 @@
 		'Lila',
 		'Laila',
 		'Luz',
+		'maria',
+		'marcela',
+		'mabel',
+		'melva',
+		'monica',
+		'molina',
+		'mario',
+		'mercedes',
+		'norma',
+		'nancy',
+		'nando',
+		'ñañe',
+		'oso',
+		'oscar',
+		'orlando',
+		'oriana',
+		'pedro',
+		'pablo',
+		'pipe',
+		'perlaza',
+		'pepito',
+		'perez',
+		'quimbaya',
+		'quiroz',
+		'queretaro',
+		'raul',
+		'rodrigo',
+		'ramiro',
+		'rosa',
+		'romario',
+		'ronaldo',
+		'rodriguez',
+		'stevens',
+		'salgado',
+		'samuel',
+		'sevilla',
+		'sami',
+		'tovar',
+		'tito',
+		'tomas',
+		'tobias',
+		'ursula',
+		'uriel',
+		'umaña',
+		'victor',
+		'valencia',
+		'varon',
+		'velez',
+		'vanegas',
+		'walter',
+		'wilson',
+		'xiomara',
+		'ximera',
+		'xilofono',
+		'xoxo',
+		'yamile',
+		'yesenia',
+		'yiyi',
+		'yuyis',
+		'zeppelin',
+		'zambrano',
+		'zeta',
+		'zion',	
+	);
+	
+	if(strlen($datosRecibidos)>0){
 
+		for( $i=0; $i < count($sugerencias); $i++){
 
-		)
+			if(strtolower($datosRecibidos) == strtolower(substr($sugerencias[$i],0,strlen($datosRecibidos)))){
+				if($sugerencia == ''){
+					$sugerencia = $sugerencias[$i];
+				}else{
+					$sugerencia = $sugerencia . ' , ' . $sugerencias[$i];
+				}
+			}
+		}
+	}
+
+	// Si no hay sugerencias se coloca esto en el Div
+	if($sugerencia == ''){
+		$sugerencia = 'No hay Sugerencias :( ';
+	}
+	// si la hay, muestra la o las sugerencias.
+	echo $sugerencia;
  ?>
